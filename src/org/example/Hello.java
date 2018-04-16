781aa35dbd6a8688b12b950702f2d1bdf80ea525
Benjamin Troller <7834154+btroller@users.noreply.github.com>
2018-04-16 15:16:52 -0700

    finally

--- src/org/example/Hello.java
+++ src/org/example/Hello.java
@@ -7,8 +7,6 @@ public class Hello {
 	private static final Logger logger = Logger.getLogger( Hello.class.getName() );
 	
 	public static void main(String[] args) {
-//		System.out.println("Hello, world!");
-//		Logger logger;
 		logger.log(Level.SEVERE, "Hello, world.");
 	}
 	
